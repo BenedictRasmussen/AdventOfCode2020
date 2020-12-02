b06a28b32890c8faf085eb3f468e16974fda3570
Ben Rasmusse <benedictrasmussen@gmail.com>
2020-12-01 17:29:06 -0700

    Day1-1 solution, experiments, and util setup
    Day 1 solved.
    
    D1P1 solved in O(N), D1P2 solved in O(O^2).
    Little messy for P2 solution but does have some code reuse. Would love to clean it and fix that nasty time complexity.
    
    Day1-1 solution, experiments, and util setup

--- src/adventofcode/day1.kt
+++ src/adventofcode/day1.kt
@@ -8,41 +8,29 @@ fun main(args: Array<String>) {
     val filename = FILEPATH + "day1-1"
 
     val input = File(filename)
-        .readLines()
-        .map { it.toInt() }
-        .toHashSet()
+            .readLines()
+            .map { it.toInt() }
+            .toHashSet()
 
-    val myLittleLambda: (HashSet<Int>, Int) -> Int = myLittleLambda@{ input, targetSum ->
+    val solver: (HashSet<Int>, Int) -> Int? = myLittleLambda@{ input, targetSum ->
         input.forEach { curr ->
             val intRequired = targetSum - curr
             if (input.contains(intRequired)) {
                 return@myLittleLambda curr * intRequired
             }
         }
-        throw NoSolutionException("No solution found to puzzle 1-1")
+        null
     }
 
-    println ("Day1-1 solution: ${day1puzzle1(input)}")
-    println ("Day1-2 solution: ${day1puzzle1(input)}")
-}
+    val solution1: Int? = solver(input, 2020)
 
-fun day1puzzle1(input: HashSet<Int>): Int {
+    println("Day1-1 solution: $solution1")
 
     input.forEach { curr ->
-        val intRequired = 2020 - curr
-        if (input.contains(intRequired)) {
-            return curr * intRequired
+        val match = solver(input, 2020 - curr)
+        if (match != null) {
+            println("Day1-2 solution: ${match * curr}")
+            return
         }
     }
-    throw NoSolutionException("No solution found to puzzle 1-1")
-}
-
-fun day1puzzle2(input: HashSet<Int>): Int {
-    input.forEach { curr ->
-        val req = 2020 - curr
-        if (input.contains(req)) {
-            return curr * req
-        }
-    }
-    throw NoSolutionException("No solution found to puzzle 1-1")
 }
