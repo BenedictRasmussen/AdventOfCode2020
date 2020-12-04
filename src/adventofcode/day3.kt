c92c91072243dde2c23d608508b68036368c187d
Ben Rasmusse <benedictrasmussen@gmail.com>
2020-12-04 10:14:02 -0700

    Day 2 complete, cleaned code

--- src/adventofcode/day3.kt
+++ src/adventofcode/day3.kt
@@ -4,15 +4,14 @@ import utils.Utilities
 
 fun main(args: Array<String>) {
     val input = Utilities.readInput("day3")
-    val pathingOptions = listOf<Pair<Int, Int>>(Pair(1, 1), Pair(3, 1), Pair(5, 1), Pair(7, 1), Pair(1, 2))
-//    val pathingOptions = listOf<Pair<Int, Int>>(Pair(1, 2))
-    var treeMult = 1
+    val pathingOptions = listOf(Pair(1, 1), Pair(3, 1), Pair(5, 1), Pair(7, 1), Pair(1, 2))
+    var treeMult: Long = 1L
+
     pathingOptions.forEach {
         val trees = treeChecker(input, it)
-        println("Pathing: $it, treesHit: $trees, treeMult before mult: $treeMult")
         treeMult *= trees
     }
-    //816992744 too low
+
     println("Trees mult: $treeMult")
 }
 
@@ -20,33 +19,32 @@ fun main(args: Array<String>) {
 fun treeChecker(input: List<String>, pathing: Pair<Int, Int>): Int {
     val iterator = input.iterator()
     var trees = 0
-    // prime the loop
-    var line = iterator.next()
-    var currPos = pathing.first
+    // Used to skip rows for certain pathing options
+    val skip = {skips: Int, iterator: Iterator<String> ->
+        var numSkips = skips
+        while (numSkips > 1 && iterator.hasNext()) {
+            numSkips--
+            iterator.next()
+        }
+    }
 
-    println(line)
+    // prime the loop
+    iterator.next()
+    var currPos = pathing.first
+    skip(pathing.second, iterator)
     while (iterator.hasNext()) {
-        line = iterator.next()
-        var output = line.toCharArray()
-        output[currPos] = 'O'
+        val line = iterator.next()
 
         if (line[currPos] == '#') {
-            output[currPos] = 'X'
             trees++
         }
 
-//        println(output)
         currPos += pathing.first
         if (currPos >= line.length) {
             currPos -= line.length
         }
 
-        var skips = pathing.second
-        while (skips > 1 && iterator.hasNext()) {
-            skips--
-            println(iterator.next())
-        }
-        println(output)
+        skip(pathing.second, iterator)
     }
     return trees
 }
